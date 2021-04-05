14fe7affc91c94c0d8fea4994802838d9b692227
Asmit <asmitsirohi9761@gmail.com>
2021-04-05 11:19:26 +0530

    organized_code

---
+++ components/common/navbar.php
@@ -0,0 +1,14 @@
+<section>
+    <nav class="navbar navbar-light navbar-expand-md fixed-top" style="font-size: 20px;background: rgba(0,0,0,0.4);">
+        <div class="container-fluid"><a class="navbar-brand" href="#" style="font-family: 'Titillium Web', sans-serif;font-size: 30px;color: rgb(254,254,254);">LesKollab</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link active text-danger" href="#" style="margin-right: 1vw;"><?= $name ?>&nbsp;<i class="fa fa-user-circle-o"></i></a></li>
+                    <li class="nav-item"><a class="nav-link" data-toggle="modal" data-target=".bd-example-modal-lg" href="#" style="color: rgb(197,189,0);margin-right: 1vw;">Post an Idea&nbsp;<i class="fa fa-lightbulb-o"></i></a></li>
+                    <li class="nav-item"><a class="nav-link" href="../logout.php" style="color: rgb(255,255,251);margin-right: 1vw;">Logout&nbsp;<i class="fa fa-sign-out-alt"></i></a></li>
+                </ul>
+                <!-- <ul class="nav navbar-nav"></ul><button class="btn openBtn" onclick="openSearch()" style="color: rgb(255,255,255);"><i class="fa fa-search" style="color: rgb(255,255,255);"></i></button>-->
+            </div>
+        </div>
+    </nav>
+</section>
