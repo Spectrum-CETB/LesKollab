310c024cadc581ad4927467b1efd4f50384243aa
Hardik <varshneyhardik98@gmail.com>
2021-04-15 09:01:55 +0530

    Added Admin Dashboard showing number of users, projects and messages from user

--- admin/admin_login.php
+++ admin/admin_login.php
@@ -93,7 +93,7 @@ else
     
     <div class="col-lg-3"></div>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.18/js/bootstrap-select.min.js"></script>
   <script src="assets/js/all.min.js"></script>
