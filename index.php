9c447a8d470c55c1d4e92786e95d4cfdfcf0db9c
arpit456jain <111arpit1@gmail.com>
2021-05-06 17:12:06 +0530

    final commit

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if (isset($_SESSION['email'])) {
   <link rel="stylesheet" href="assets/css/Button-Ripple-Effect-Animation-Wave-Pulse.css">
   <link rel="stylesheet" href="assets/css/styles.css">
   <link rel="stylesheet" href="assets/css/darkmode.css">
-  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
   <!--Font Awesome CSS-->
   <link rel="stylesheet" href="assets/css/all.min.css">
 
